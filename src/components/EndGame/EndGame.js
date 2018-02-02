f121f21600c7a7410ca20b278c17129911144dec
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-02-02 18:30:39 +0300

    Сделал экран с подсчетом очков

---
+++ src/components/EndGame/EndGame.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './EndGame.css';
+
+import image from '../../img/Group 2@2x.png'
+
+class EndGame extends Component {
+  render() {
+    return (
+      <div className="endgame">
+        <img className="endgame__image" src={image} />
+        <h1 className="endgame__name">Поздравляем! <br /> Ваш итоговый счет: 666</h1>
+      </div>
+    );
+  }
+}
+
+export default EndGame;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import StartGame from '../components/StartGame/StartGame.js'
+import EndGame from '../components/EndGame/EndGame.js'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <StartGame />
+        <EndGame />
       </div>
     );
   }
